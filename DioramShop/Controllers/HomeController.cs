fee7dff1538a3eba3bb635f241d394b9debc7b95
APrimikirio <a.d.primikirio@gmail.com>
2018-09-29 21:16:56 +0300

    Controllers experiments

--- DioramShop/Controllers/HomeController.cs
+++ DioramShop/Controllers/HomeController.cs
@@ -17,5 +17,22 @@ namespace DioramShop.Controllers
             ViewBag.Products = products;
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.ProductId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
     }
 }
